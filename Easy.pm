66873600b4d19dbb5d8fd30231dde8da8b5b930b
Paul Miller <jettero@cpan.org>
2010-07-30 07:36:30 -0400

    version up

--- Easy.pm
+++ Easy.pm
@@ -16,7 +16,7 @@ use NetPacket::UDP;
 use NetPacket::IGMP;
 use NetPacket::ICMP qw(:types);
 
-our $VERSION     = "1.4200";
+our $VERSION     = "1.4205";
 our $MIN_SNAPLEN = 256;
 our $DEFAULT_PPL = 32;
 
